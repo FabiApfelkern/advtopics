a35825431dc8eb10be1e2bed6fc9ea2f9138c660
Fabi Kirstein <fabi.kirstein@gmail.com>
2013-09-16 23:02:33 +0200

    Add Selenium Test and its Maven integration

---
+++ src/test/scala/advtopics/selenium/FrontendTest.scala
@@ -0,0 +1,40 @@
+package advtopics.selenium
+
+import org.junit._
+import org.junit.Assert._
+import org.openqa.selenium.By
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebElement
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.htmlunit.HtmlUnitDriver
+import org.openqa.selenium.support.ui.ExpectedConditions
+import org.openqa.selenium.support.ui.WebDriverWait
+import junit.framework.Assert
+
+class FrontendTest {
+  
+  def url = "http://localhost:8080/beleg/"
+  var driver:WebDriver = null
+  
+  @Before
+  def init: Unit = {
+    driver = new HtmlUnitDriver
+    driver.get(url)
+    (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.tagName("title"))) 	
+    
+  }
+
+  @Test
+  def HomepageTest = {
+    driver.get(url)
+    val title = driver.getTitle()
+    assertEquals("TrendCloud", title)
+
+  }
+
+  @After
+  def after: Unit = {
+    driver.quit
+  }
+  
+}
