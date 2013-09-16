dd8e0268a09a2d766eed6f0032e962f0367da638
Bjoern Ahlfeld <b.ahlfeld@gmail.com>
2013-09-16 22:37:00 +0200

    Soundcloud is wotking with a few fields

--- src/main/scala/advtopics/trendcloud/models/SoundcloudTrack.scala
+++ src/main/scala/advtopics/trendcloud/models/SoundcloudTrack.scala
@@ -9,23 +9,24 @@ import org.codehaus.jackson.annotate.JsonIgnoreProperties
 case class SoundcloudTrack(
   val id: Integer, // val kind:String,
   val user_id: Integer,
- /* val duration: Integer,
-  val commentable: Boolean,
-  val state: String,
-  val original_content_size: Integer,
-  val purchase_title: String,
-  val genre: String,
+ //val duration: Integer,
+  //val commentable: Boolean,
+  //val state: String,
+  //val original_content_size: Integer,
+  //val purchase_title: String,
+  //val genre: String,
   val title: String,
-  val description: String,
-  val uri: String,*/
-  val user: SoundcloudUser,
-  val permalink_url: String,
-  val artwork_url: String,
-  val waveform_url: String,
-  val stream_url: String,
-  val playback_count: Integer,
-  val download_count: Integer,
-  val favoritings_count: Integer,
-  val comment_count: Integer,
-  val attachments_uri: String) {
+  //val description: String,
+  val uri: String,
+  //val user: SoundcloudUser,*/
+  //val permalink: String,
+  //val artwork_url: String,
+  //val waveform_url: String,*/
+  //val stream_url: String,
+  //val playback_count: Integer,
+  //val download_count: Integer,
+  //val favoritings_count: Integer,
+  //val comment_count: Integer,
+  val attachments_uri: String
+  ) {
 }
